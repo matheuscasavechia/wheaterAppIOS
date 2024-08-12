278a737dfea5efc79374dc92cb3b04c0448eab4a
matheuscasavechia <matheuscasavechia@Matheuss-MacBook-Air.local>
2024-08-12 14:45:44 -0300

    feat: testing commit

--- WeatherDemo/WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/WeatherDemo/Views/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
-    
+
     var weatherManager = WeatherManager()
     @State var weather: ResponseBody?
-    
-    
+
+    // testing commit
     var body: some View {
         VStack{
-    
+
             if let location = locationManager.location {
                 if let weather = weather{
                     WeatherView(weather: weather)
